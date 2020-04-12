22ccec27e310bebd33ff7756f2c4ccc9ee3912dc
PanXiebit <ftdpanxie@gmail.com>
2020-04-12 07:53:53 +0000

    add en-de

--- generate_cmlm.sh
+++ generate_cmlm.sh
@@ -1,15 +1,21 @@
 output_dir=output
 src=en
-tgt=ro
+tgt=de
 model_path=output
+model_dir=${model_path}/my_maskPredict_${src}_${tgt}
 
-model_dir=${model_path}/my_maskPredict_loss_${src}_${tgt}
 
+# python generate_cmlm.py output/data-bin/wmt14.${src}-${tgt} \
+#     --path ${model_dir}/checkpoint5.pt \
+#     --task translation_self \
+#     --remove-bpe \
+#     --max-sentences 20 \
+#     --decoding-iterations 15 \
+#     --decoding-strategy mask_predict
 
-python generate_cmlm.py ${output_dir}/data-bin \
-    --path ${model_dir}/checkpoint5.pt \
-    --task translation_self \
-    --remove-bpe \
-    --max-sentences 20 \
-    --decoding-iterations 15 \
-    --decoding-strategy mask_predict
+# mkdir generate_fuse_log
+for i in {15..19}
+do
+  python generate_cmlm.py output/data-bin/wmt14.${src}-${tgt}  --path output/my_maskPredict_en_ro/checkpoint${i}.pt --task translation_self --remove-bpe --max-sentences 20 --decoding-iterations 15 --decoding-strategy mask_predict > generate_fuse_log/epoch_${i}_iter_15.log
+  echo "Finished Epoch ${i}."
+done
